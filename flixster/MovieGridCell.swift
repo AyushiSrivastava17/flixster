3451a388b9541b3dead5318623e6e8de12b8b987
Ayushi Srivastava <ayushisri@gmail.com>
2019-04-20 23:15:24 -0700

    Added extra features to the Flixster App. Now can look at specific movies related to Superheros in the Top Bar

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Ayushi Srivastava on 4/20/19.
+//  Copyright © 2019 ayushisri. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
